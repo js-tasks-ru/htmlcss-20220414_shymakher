1f7b3c0a50cda0552f42b654cf9b9c7e12a24463
vladimir@dueto.io <vladimir@dueto.io>
2022-04-27 17:42:53 +0300

    modal - part 1

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,19 +1,25 @@
-(function() {
-    const close = document.getElementById('modal-close');
-    const modal = document.getElementById('modal');
-    const modalOpen = document.getElementById('modal-open');
+(function () {
+  const close = document.getElementById('modal-close');
+  const modal = document.getElementById('modal');
+  const modalOpen = document.getElementById('modal-open');
 
-    if (!close || !modalOpen || !modal) {
-        return;
-    }
+  if (!close || !modalOpen || !modal) {
+    return;
+  }
 
-    modalOpen.addEventListener('click', () => {
-        modal.style.display = 'block';
-        document.body.overflow = 'hidden';
-    });
+  modalOpen.addEventListener('click', () => {
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    document.body.style.position = 'relative';
+  });
 
-    close.addEventListener('click', () => {
-        modal.style.display = 'none';
-        document.body.overflow = 'initial';
-    });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'initial';
+  });
+
+//   modal.addEventListener('click', () => {
+//     modal.style.display = 'none';
+//     document.body.style.overflow = 'initial';
+//   });
 })();
