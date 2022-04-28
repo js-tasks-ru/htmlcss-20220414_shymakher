1492f5654cf253afb3c38620c87fbce688016f89
vladimir@dueto.io <vladimir@dueto.io>
2022-04-28 13:37:11 +0300

    modal after code review

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,6 +1,7 @@
 (function () {
   const close = document.getElementById('modal-close');
   const modal = document.getElementById('modal');
+  const modalContent = document.getElementById('modal-content');
   const modalOpen = document.getElementById('modal-open');
 
   if (!close || !modalOpen || !modal) {
@@ -10,7 +11,6 @@
   modalOpen.addEventListener('click', () => {
     modal.style.display = 'block';
     document.body.style.overflow = 'hidden';
-    document.body.style.position = 'relative';
   });
 
   close.addEventListener('click', () => {
@@ -18,8 +18,13 @@
     document.body.style.overflow = 'initial';
   });
 
-//   modal.addEventListener('click', () => {
-//     modal.style.display = 'none';
-//     document.body.style.overflow = 'initial';
-//   });
+  modal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'initial';
+  });
+
+  // close modal by clicking on the grey modal`s background
+  modalContent.addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
 })();
